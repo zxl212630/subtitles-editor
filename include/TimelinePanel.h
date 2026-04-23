68daacad7ae30c3a358bb26795d6bb41d94b41ed
zxl212630 <zxl212630@gmail.com>
2026-04-23 09:48:36 +0800

    feat(panel): add TimelinePanel with paintEvent rendering

---
+++ include/TimelinePanel.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QWidget>
+
+class SubtitleTrack;
+
+class TimelinePanel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit TimelinePanel(QWidget* parent = nullptr);
+
+    void setTrack(SubtitleTrack* track);
+
+signals:
+    void timeClicked(qint64 ms);
+    void itemSelected(const QString& id);
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+
+private:
+    void drawRuler(QPainter& painter);
+    void drawSubtitleTrack(QPainter& painter, int y);
+    void drawVideoTrack(QPainter& painter, int y);
+    void drawPlayhead(QPainter& painter);
+
+    qint64 pixelsToMs(int px) const;
+    int msToPixels(qint64 ms) const;
+
+    SubtitleTrack* track_ = nullptr;
+    qint64 totalDurationMs_ = 11000; // placeholder 11 seconds
+    qint64 currentTimeMs_ = 6040;    // placeholder ~6 seconds
+    static constexpr int RULER_HEIGHT = 36;
+    static constexpr int SUBTITLE_TRACK_HEIGHT = 48;
+    static constexpr int VIDEO_TRACK_HEIGHT = 96;
+    static constexpr int TRACK_HEAD_WIDTH = 120;
+    static constexpr int PIXELS_PER_SECOND = 100;
+};
