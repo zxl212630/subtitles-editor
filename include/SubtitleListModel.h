f8f650dccebf0142cecc089f6f0fd6c634bb0d95
zxl212630 <zxl212630@gmail.com>
2026-04-23 09:43:18 +0800

    feat(model): add SubtitleListModel with filtering

---
+++ include/SubtitleListModel.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <QAbstractListModel>
+#include <QString>
+#include <QList>
+
+class SubtitleTrack;
+
+class SubtitleListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum Roles {
+        IdRole = Qt::UserRole + 1,
+        TextRole,
+        StartMsRole,
+        EndMsRole,
+        SelectedRole,
+        StartTimeRole,
+        EndTimeRole
+    };
+
+    explicit SubtitleListModel(QObject* parent = nullptr);
+
+    void setTrack(SubtitleTrack* track);
+    void setFilterText(const QString& text);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+private slots:
+    void onDataChanged();
+
+private:
+    void rebuildFilteredIndices();
+    static QString formatTime(qint64 ms);
+
+    SubtitleTrack* track_ = nullptr;
+    QString filterText_;
+    QList<int> filteredIndices_;
+};
